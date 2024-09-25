a90173a6b72155c383fd12298cce7919cbcff028
Krishna4401 <businesskk4401@gmail.com>
2024-09-25 18:32:08 +0530

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,10 @@
+a="Twinkle, twinkle, little star"
+b='''JavaScript is a versatile, high-level programming language that's widely used for web development. Here’s a comprehensive overview of its core concepts:1. Basic Syntax and OperatorsVariables'''
+print("Poem :",a)
+print( b)
+
+C=10
+D=20
+print(C&D)
+
+
